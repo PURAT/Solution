e48968ddce873ab28e3574684868471c3f60672f
shtunder <shtunder.andrey1@yandex.by>
2020-03-21 19:44:58 +0300

    Made rafactoring register.

--- app/src/main/java/ru/startandroid/booknet/models/User.java
+++ app/src/main/java/ru/startandroid/booknet/models/User.java
@@ -7,22 +7,58 @@ import java.util.List;
 
 import ru.startandroid.booknet.models.Book;
 
-public class User{
-    private  String name;
+public class User {
+    private String name;
+    private String surname;
     private int age;
+    private String password;
+    private String email;
+
+    private List<String> interestsList = new ArrayList<>();
+    private List<Book> favoriteList = new ArrayList<>();
     private List<Book> userStoryList = new ArrayList<>();
+
+    public User() {
+    }
 
     public User(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public List<String> getInterestsList() {
         return interestsList;
     }
-
-    private List<String> interestsList = new ArrayList<>();
-    private List<Book> favoriteList = new ArrayList<>();
 
     public String getName() {
         return name;
@@ -36,18 +72,20 @@ public class User{
         return userStoryList;
     }
 
-    public void rateBook(Book book, int rate){
+    public void rateBook(Book book, int rate) {
         book.getRateData().add(rate);
         int sum = 0;
-        for(Integer i: book.getRateData()){
-            sum+=i;
+        for (Integer i : book.getRateData()) {
+            sum += i;
         }
-        book.setRating(sum/book.getRateData().size());
+        book.setRating(sum / book.getRateData().size());
     }
-    public void markAsFavotite(Book book){
+
+    public void markAsFavotite(Book book) {
         this.favoriteList.add(book);
     }
-    public boolean isFavorite(Book book){
+
+    public boolean isFavorite(Book book) {
         return this.favoriteList.contains(book);
     }
 
